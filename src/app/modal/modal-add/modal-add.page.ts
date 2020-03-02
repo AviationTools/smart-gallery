93cb6949234ac92abe7e4336f2dba48eed947d14
AviationTools <werbungbnc@web.de>
2020-03-02 20:31:41 +0100

    added Modal part 2

---
+++ src/app/modal/modal-add/modal-add.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-modal-add',
+  templateUrl: './modal-add.page.html',
+  styleUrls: ['./modal-add.page.scss'],
+})
+export class ModalAddPage implements OnInit {
+
+  constructor(public modalController: ModalController) { }
+
+  ngOnInit() {
+  }
+
+  dismissModal() {
+    // using the injected ModalController this page
+    // can "dismiss" itself and optionally pass back data
+    this.modalController.dismiss({
+      'dismissed': true
+    });
+  }
+
+}
